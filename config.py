6f03d975bd7e040347c9542af95909ff5b354eb9
Buddy <bgmiduniya2006@gmail.com>
2025-06-27 23:54:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/karmaxexclusive/ZyroMusic",
+    "https://github.com/Ahad611/ZyroMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
