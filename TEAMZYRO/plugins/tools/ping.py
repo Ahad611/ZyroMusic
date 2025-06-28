6b4e3bbfb8387bd004baa3f4cb7e47913b95a52f
Buddy <bgmiduniya2006@gmail.com>
2025-06-28 10:42:16 +0530

    Update ping.py

--- TEAMZYRO/plugins/tools/ping.py
+++ TEAMZYRO/plugins/tools/ping.py
@@ -1,8 +1,7 @@
 from datetime import datetime
-
 from pyrogram import filters
 from pyrogram.types import Message
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from config import *
 from TEAMZYRO import app
 from TEAMZYRO.core.call import ZYRO
@@ -11,19 +10,19 @@ from TEAMZYRO.utils.decorators.language import language
 from TEAMZYRO.utils.inline import supp_markup
 from config import BANNED_USERS
 
-
 @app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
-    start = datetime.now()
-    response = await message.reply_video(
-        video="https://graph.org/file/5690109178f081adf464d.mp4",
-        caption=_["ping_1"].format(app.mention),
-    )
-    pytgping = await ZYRO.ping()
-    UP, CPU, RAM, DISK = await bot_sys_stats()
-    resp = (datetime.now() - start).microseconds / 1000
-    await response.edit_text(
-        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
-        reply_markup=supp_markup(_),
-    )
+    start = datetime.now()
+    response = await message.reply_video(
+        video="https://graph.org/file/5690109178f081adf464d.mp4",
+        caption=_["ping_1"].format(app.mention),
+    )
+    pytgping = await ZYRO.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+
+    await response.edit_caption(
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(_),
+    )
